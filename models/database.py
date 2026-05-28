0a9dbf368ef00f3527ebfe559f27fb6af8368f0b
Yulia Dolishnia <yuliia.dolishnia@gmail.com>
2026-05-29 00:04:15 +0300

    fix: set database to sqlite for lab3

--- models/database.py
+++ models/database.py
@@ -2,9 +2,15 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 
-DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres_admin:password123@localhost:5432/library_db")
+# Змінюємо дефолтне значення на локальний SQLite для тестування
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./library.db")
 
-engine = create_engine(DATABASE_URL)
+# Додаємо аргумент connect_args, який обов'язковий для SQLite
+if DATABASE_URL.startswith("sqlite"):
+    engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+else:
+    engine = create_engine(DATABASE_URL)
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
