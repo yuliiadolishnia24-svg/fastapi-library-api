731efcf4be25d8a9cd9de79abb335b1226329941
Yulia Dolishnia <yuliia.dolishnia@gmail.com>
2026-05-29 00:14:03 +0300

    fix: set database back to sqlite for jwt auth in lab6

--- models/database.py
+++ models/database.py
@@ -1,12 +1,21 @@
 import os
-import motor.motor_asyncio
+from sqlalchemy import create_engine
+from sqlalchemy.orm import declarative_base, sessionmaker
 
-# Беремо URL з енвайронменту Docker, або локальний за замовчуванням
-MONGO_URL = os.getenv("MONGO_URL", "mongodb://mongo_admin:password123@localhost:27017")
+# Беремо URL з оточення або використовуємо локальний SQLite за замовчуванням
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./library.db")
 
-client = motor.motor_asyncio.AsyncIOMotorClient(MONGO_URL)
-db = client["library_db"]
+if DATABASE_URL.startswith("sqlite"):
+    engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+else:
+    engine = create_engine(DATABASE_URL)
 
-# Функція (Dependency) для отримання доступу до колекції книг
-def get_books_collection():
-    return db["books"]
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
